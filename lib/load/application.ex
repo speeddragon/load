2809cba6eb0751016ebce39d40fa213263399f70
Bruno Ripa <bruno.ripa@gmail.com>
2022-01-15 09:47:27 +0000

    Work in progress

--- lib/load/application.ex
+++ lib/load/application.ex
@@ -1,4 +1,4 @@
-defmodule Load.Applicaiton do
+defmodule Load.Application do
   use Application
 
   @impl true
@@ -20,5 +20,20 @@ defmodule Load.Applicaiton do
     :folsom_metrics.new_counter(:http_errors)
     :folsom_metrics.new_counter(:unconfirmed_transactions)
     :folsom_metrics.new_counter(:running_clients)
+  end
+
+  @doc """
+  This function exposes a map which is supposed to be provided in the
+  application configuration, in the form:
+
+      ```
+      config :app, message_types: %{
+        "audit_event_1" => implementation_module_1,
+        "audit_event_2" => implementation_module_2
+      }
+      ```
+  """
+  def get_simulator_implementation(message_type) do
+    Application.get_env(:load, :message_types)
   end
 end
