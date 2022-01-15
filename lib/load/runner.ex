4e55308c9b334bc96971de00877da943d8b2e7f7
Dominic Perini <dominic.perini@gmail.com>
2022-01-15 18:32:35 +0000

    up

--- lib/load/runner.ex
+++ lib/load/runner.ex
@@ -41,62 +41,79 @@ defmodule Load.Runner do
     if length(running_workers) > count do
       stop_workers(count, Enum.map(running_workers, &elem(&1, 1)))
     end
-  end)
-end
+    end)
+  end
+
+  def on_worker_started(node_id, pid) do
+    Logger.info("[#{__MODULE__}] worker #{node_id}/#{pid} started")
+    :ets.insert(@workers_tab, {node_id, pid})
+  end
 
 
-defp setup_nodes(config) do
-  :ets.new(@nodes_tab, [:named_table])
-
-  Keyword.get(config, :load_nodes, [])
-  |> Enum.each(fn node_config ->
-     node_id = Keyword.get(node_config, :id)
-     :ets.insert(@nodes_tab, {node_id, node_config})
-     Logger.info("Customer node (as map): #{inspect(Enum.into(node_config, %{}))}")
-
-  end)
-
-  config
-end
-
-defp setup_workers(config) do
-  Keyword.get(config, :load_nodes, [])
-  |> Enum.each(fn _ -> create_workers_for_node(config) end)
-
-  config
-end
+  def on_worker_terminated(node_id, pid, reason) do
+    Logger.error("[#{__MODULE__}] worker #{node_id}/#{pid} terminated for reason: #{reason}")
+    ms = :ets.fun2ms(fn {_Node, p} -> p == pid end)
+    :ets.select_delete(@workers_tab, ms)
+  end
 
 
-defp create_workers_for_node(config) do
+  def handle_info({EXIT, pid, reason}, state) do
+    Process.unlink(pid)
+    on_worker_terminated(:unknown, pid, reason)
+    {:noreply, state}
+  end
 
-  pids = Enum.map(1..Keyword.get(config, :workers_per_node), fn _ ->
-    # {:ok, pid} = Supervisor.start_child(:load_worker_sup, [configt])
-    # Process.link(pid)
-    # pid
-    :ok
-  end)
+  defp setup_nodes(config) do
+    :ets.new(@nodes_tab, [:named_table])
 
-  node_id = config[:node_config].id
-  count = length(pids)
-  Logger.info("[#{__MODULE__}] spawned #{count} workers on node: #{node_id}")
+    Keyword.get(config, :load_nodes, [])
+    |> Enum.each(fn node_config ->
+      node_id = Keyword.get(node_config, :id)
+      :ets.insert(@nodes_tab, {node_id, node_config})
+      Logger.info("Customer node (as map): #{inspect(Enum.into(node_config, %{}))}")
+    end)
 
-  pids
-end
+    config
+  end
 
-defp stop_workers(count, running_workers), do:
-  Enum.reduce(1..count, running_workers, fn (_n, [pid | more]) -> send(pid, :stop); more end)
+  defp setup_workers(config) do
+    Keyword.get(config, :load_nodes, [])
+    |> Enum.each(fn _ -> create_workers_for_node(config) end)
+
+    config
+  end
 
 
-def print_usage() do
-  IO.puts(
-    "+-----------------------------------------------\n"
-  <>"| Commands available:\n"
-  <>"| Load.add   ('all'| node_id, workers) - increase workers for node_id or all\n"
-  <>"| Load.remove('all'| node_id, workers) - reduce   workers for node_id or all\n"
-  <>"| Load.stop() - <describe>\n"
-  <>"| Load:i() - print current stats\n"
-  <>"+-----------------------------------------------"
-  )
-end
+  defp create_workers_for_node(config) do
+
+    pids = Enum.map(1..Keyword.get(config, :workers_per_node), fn _ ->
+      # {:ok, pid} = Supervisor.start_child(:load_worker_sup, [configt])
+      # Process.link(pid)
+      # pid
+      :ok
+    end)
+
+    node_id = config[:node_config].id
+    count = length(pids)
+    Logger.info("[#{__MODULE__}] spawned #{count} workers on node: #{node_id}")
+
+    pids
+  end
+
+  defp stop_workers(count, running_workers), do:
+    Enum.reduce(1..count, running_workers, fn (_n, [pid | more]) -> send(pid, :stop); more end)
+
+
+  def print_usage() do
+    IO.puts(
+      "+-----------------------------------------------\n"
+    <>"| Commands available:\n"
+    <>"| Load.add   ('all'| node_id, workers) - increase workers for node_id or all\n"
+    <>"| Load.remove('all'| node_id, workers) - reduce   workers for node_id or all\n"
+    <>"| Load.stop() - <describe>\n"
+    <>"| Load:i() - print current stats\n"
+    <>"+-----------------------------------------------"
+    )
+  end
 
 end
