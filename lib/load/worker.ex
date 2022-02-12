fc5826140a278d4ba0361fc48ffad0c9c24d8c0c
Dominic Perini <dominic.perini@gmail.com>
2022-02-12 11:59:33 +0000

    worker WIP

--- lib/load/worker.ex
+++ lib/load/worker.ex
@@ -6,129 +6,85 @@ defmodule Load.Worker do
 
   require Logger
 
-# state {
-#     ref = erlang:ref_to_list(erlang:make_ref()),
-#     inject_conn :: pid(), % Gun HTTP connection to inject entries.
-#     node_id :: string(),
-#     base_url,
-#     headers :: list(),
-#     sleep_time,
-#     accum = {0, []} :: {integer(), any()},
-#     stats_last_ms = 0, % when last stats were sent (milliseconds)
-#     stats_reqs = 0,
-#     stats_entries = 0,
-#     stats_errors = 0,
-# }
+  @connect_delay 200
+  @retry_interval :timer.seconds(20)
 
   def start_link(glob, args \\ []), do: GenServer.start_link(__MODULE__, glob ++ args |> Enum.into(%{}) )
 
-  # def init(%{
-  #   transport: transport,
-  #   config: config,
-  #   max_sleep: max_sleep
-  # } = state) do
-  #   host = Keyword.get(config, :host)
-  #   host_ip =
-  #     case :inet.getaddr(host, :inet) do
+  def init(args) do
 
-  #       {:ok, ip} -> ip
-  #       {:error, reason} ->
-  #         Logger.error("[#{__MODULE__}] init failed for host:#{inspect(host)} due to:#{inspect(reason)}")
-  #         # TODO: check if this is just stopping the worker or something more
-  #         # :init.stop()
-  #         :timer.sleep(:timer.seconds(20))
-  #     end
-  #   Logger.debug("#{__MODULE__}:#{inspect(self())} targets ip:#{inspect(host_ip)} - host:#{inspect(host)}")
-  #   port = Keyword.get(config, :port)
+    Logger.debug("init called with args: #{inspect(args)}")
 
-  #   http_opts =
-  #     case transport do
-  #       "http" -> %{}
-  #       "https" -> %{transport: :ssl}
-  #     end
+    host = Map.get(args, :host)
+    port = Map.get(args, :port)
+    opts = Map.get(args, :opts)
+    sim = Map.get(args, :sim)
+    hit_interval = Map.get(args, :hit_interval)
 
+    Process.send_after(self(), :connect, @connect_delay)
 
-  #   conn = create_connection(host_ip, port, http_opts)
-  #   headers = [{"accept", "application/json"},
-  #               {"content-type", "application/json"}]
-  #   # Registering the worked in the :ets table
-  #   Load.Stats.register_worker(self())
-  #   :folsom_metrics.notify({:running_clients, {:inc, 1}})
-  #   #Load.Runner.on_worker_started(__MODULE__, :node_id, self())
+    state = %{
+      host: host,
+      port: port,
+      opts: opts,
+      sim: sim,
+      hit_interval: hit_interval,
+      stats_last_ms: 0, # when last stats were sent (milliseconds)
+      stats_reqs: 0,
+      stats_entries: 0,
+      stats_errors: 0,
+    }
 
-  #   Process.send(self(), :loop, [])
+    {:ok, state}
+  end
 
-  #   state = %{
-  #     node_id: :node_id,
-  #     headers: headers,
-  #     sleep_time: max_sleep,
-  #     conn: conn,
-  #     stats_last_ms: 0
-  #   } |> Map.merge(state)
-  #   {:ok, state}
+  # def handle_info(:get_ip, %{host: host} = state) do
+
+  #   case :inet.getaddr(host, :inet) do
+
+  #     {:ok, ip} ->
+  #       Process.send_after(self(), :connect, 0)
+  #       {:noreply, Map.put(state, :ip, ip)}
+
+  #     {:error, reason} ->
+  #       Logger.error("[#{__MODULE__}] init failed for host:#{inspect(host)} due to:#{inspect(reason)}")
+  #       Process.send_after(self(), :get_ip, @connect_delay)
+  #   end
+
   # end
 
-  def init(args) do
-    Logger.warn("init called with args: #{inspect(args)}")
-    {:ok, %{}}
+
+  def handle_info(:connect, %{host: host, port: port, opts: opts} = state) do
+
+    # TODO handle as case
+    {:ok, conn} = :gun.open(host, port, opts)
+    {:ok, _transport} = :gun.await_up(conn)
+
+    Process.send_after(self(), :hit, 0)
+
+    {:noreply, Map.put(state, :conn, conn)}
+
+    # case :gun.await_up(conn, @gun_timeout) do
+    #     {:ok, _} ->
+    #         conn
+    #     {:error, :timeout} ->
+    #         :timer.sleep(:timer.seconds(2))
+    #         create_connection(host_ip, port, http_opts, max_retries - 1)
+    #     error ->
+    #       Logger.error("Could not connect to host:#{inspect(host_ip)} port:#{inspect(port)} due to:#{inspect(error)}")
+    #         error
+    # end
+
   end
 
-  def handle_info(:loop, state), do: loop(state)
 
-
-# terminate(_Reason, %{node_id: node_id}) ->
-#     Load.Runner:on_worker_terminated(__MODULE__, node_id, self()).
-
-
-# Myabe these could come from config
-  @max_retries 5
-  @gun_timeout :timer.seconds(30)
-  @req_timeout :timer.seconds(30)
-
-  defp create_connection(host_ip, port, http_opts), do:
-    create_connection(host_ip, port, http_opts, @max_retries)
-
-
-  defp create_connection(host_ip, port, _http_opts, 0) do
-    Logger.error("Could not connect to host:#{inspect(host_ip)} port:#{inspect(port)} due to timeout")
-    {:error, :timeout}
+  def handle_info(:hit, %{sim: sim, hit_interval: hit_interval} = state) do
+    state = sim.process(state)
+    Process.send_after(self(), :hit, hit_interval)
+    {:noreply, state}
   end
 
-  defp create_connection(host_ip, port, http_opts, max_retries) do
-    {:ok, conn} = :gun.open(host_ip, port, http_opts)
-    case :gun.await_up(conn, @gun_timeout) do
-        {:ok, _} ->
-            conn
-        {:error, :timeout} ->
-            :timer.sleep(:timer.seconds(2))
-            create_connection(host_ip, port, http_opts, max_retries - 1)
-        error ->
-          Logger.error("Could not connect to host:#{inspect(host_ip)} port:#{inspect(port)} due to:#{inspect(error)}")
-            error
-    end
-  end
-
-  defp loop(%{
-    base_url: base_url,
-    ref: _ref_value,
-    node_id: _node_id,
-    conn: conn,
-    headers: headers,
-    stats_reqs: _stats_reqs} = state
-    ) do
-
-    # Leaving this out assuming it's message independent. If it's not, we'll
-    # move in the behaviour implementation
-    state = maybe_send_stats(state)
-
-    # Provided we have a way to "understand" what message we received, we can trigger the
-    # right module
-    message_type = "audit_event_happened"
-    module = Application.get_simulator_implementation(message_type)
-
-    payload=module.process(state)
-    state = update_state(state)
-
+  defp cacca() do
     # WE might want to hyde :gun behind a behaviour
     post_ref = :gun.post(conn, base_url <> "/entry", headers, payload)
     :folsom_metrics.notify({:sent_transactions, {:inc, 1}})
@@ -201,4 +157,10 @@ defmodule Load.Worker do
 
   defp update_state(%{stats_reqs: stats_reqs} = state), do:
     Map.put(state, :stats_reqs, stats_reqs + 1)
+
+
+  # terminate(_Reason, %{node_id: node_id}) ->
+#     Load.Runner:on_worker_terminated(__MODULE__, node_id, self()).
+
+
 end
