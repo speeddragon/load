9912f6f003ebeb6d5d907c9d34e0ec34cec72498
Miquel <miquel.garcia@betika.com>
2022-06-08 13:37:14 +0200

    Fixed :ets not used and stats integration issues
    This commit also adds config and updates `mix.lock`.

--- lib/example/packet_sim.ex
+++ lib/example/packet_sim.ex
@@ -3,17 +3,22 @@ defmodule Example.PacketSim do
 
   require Logger
 
+  @prepare_execution_condition """
+  \x11\x7b\x43\x4f\x1a\x54\xe9\x04\x4f\x4f\x54\x92\x3b\x2c\xff\x9e\
+  \x4a\x6d\x42\x0a\xe2\x81\xd5\x02\x5d\x7b\xb0\x40\xc4\xb4\xc0\x4a\
+  """
+
   @impl true
   def run(state) do
     data = %{
       "amount" => 107,
       "data" => "",
       "destination" => "example.alice",
-      "execution_condition" => "",
+      "execution_condition" => @prepare_execution_condition,
       "expires_at" => "2023-06-07T20:48:42.483Z"
     }
 
-    payload = IlpPacket.encode_prepare(data)
+    {:ok, payload} = IlpPacket.encode_prepare(data)
 
     {:ok, res_payload, new_state} =
       Load.Worker.hit(
